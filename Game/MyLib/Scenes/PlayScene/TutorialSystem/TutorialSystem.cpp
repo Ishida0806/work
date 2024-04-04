42641d3212dfbd9809fd5085221f30156e8c7c77
Ishida0806 <it222020@trident.ac.jp>
2024-04-04 11:58:05 +0900

    チュートリアルシステムの実装

---
+++ Game/MyLib/Scenes/PlayScene/TutorialSystem/TutorialSystem.cpp
@@ -0,0 +1,54 @@
+//
+//		File Name ： TutorialSystem.cpp
+//		Production： 2024/4/04
+//		Author　　： 石田 恭一	
+// 
+//		チュートリアル処理の記述をまとめたクラス
+//	
+#include "pch.h"
+#include "TutorialSystem.h"
+
+/// <summary>
+/// コンストラクタ
+/// </summary>
+TutorialSystem::TutorialSystem()
+{
+}
+
+/// <summary>
+/// デストラクタ
+/// </summary>
+TutorialSystem::~TutorialSystem()
+{
+}
+
+/// <summary>
+/// 初期化処理
+/// </summary>
+void TutorialSystem::Initialize()
+{
+
+}
+
+/// <summary>
+/// 更新処理
+/// </summary>
+/// <param name="timer">タイマー</param>
+void TutorialSystem::Update(const DX::StepTimer& timer)
+{
+
+}
+
+/// <summary>
+/// 描画処理
+/// </summary>
+void TutorialSystem::Render()
+{
+}
+
+/// <summary>
+/// 終了処理
+/// </summary>
+void TutorialSystem::Finalize()
+{
+}
--- Game/MyLib/Scenes/PlayScene/TutorialSystem/TutorialSystem.h
+++ Game/MyLib/Scenes/PlayScene/TutorialSystem/TutorialSystem.h
@@ -1,10 +1,41 @@
-
-
+//
+//		File Name ： TutorialSystem.h
+//		Production： 2024/4/04
+//		Author　　： 石田 恭一	
+//	
 #pragma once
-
 
 class TutorialSystem
 {
+
+	/// <summary>
+	/// コンストラクタ
+	/// </summary>
 	TutorialSystem();
+
+	/// <summary>
+	/// デストラクタ
+	/// </summary>
 	~TutorialSystem();
+
+	/// <summary>
+	/// 初期化処理
+	/// </summary>
+	void Initialize();
+	
+	/// <summary>
+	/// 更新処理
+	/// </summary>
+	/// <param name="timer">タイマー</param>
+	void Update(const DX::StepTimer& timer);
+
+	/// <summary>
+	/// 描画処理
+	/// </summary>
+	void Render();
+
+	/// <summary>
+	/// 終了処理
+	/// </summary>
+	void Finalize();
 };
