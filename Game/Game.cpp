47a322b5549bc5ccb635951c179d703f6fbb9249
Ishida0806 <it222020@trident.ac.jp>
2024-04-17 11:02:38 +0900

    競合解決

--- Game/Game.cpp
+++ Game/Game.cpp
@@ -322,7 +322,7 @@ void Game::CreateProjection()
 /// </summary>
 void Game::CreateScene()
 {
-   //m_sceneManager->ChangeScene<TitleScene>(this);
+   m_sceneManager->ChangeScene<TitleScene>(this);
    //m_sceneManager->ChangeScene<StageSelectScene>(this);
    m_sceneManager->ChangeScene<PlayScene>(this, m_screens);
    //m_sceneManager->ChangeScene<ResultScene>(this);
